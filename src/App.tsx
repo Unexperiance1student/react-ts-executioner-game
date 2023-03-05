a43a1f5af67aebd194b2be139d4c82efa64ed269
Vladislav <75188379+Unexperiance1student@users.noreply.github.com>
2023-03-05 03:56:25 +0300

    докурил
    сделал весь функционал

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,62 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { HangmanDrawing, HangmanWord, Keyboard } from './components';
 import words from './wordList.json';
 
-function App() {
-  const [wordToGuess, setWordToGuess] = useState<string>(() => {
-    return words[Math.floor(Math.random() * words.length)];
-  });
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
 
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(getWord);
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
-  const [] = useState();
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
+
+  const isLoser = incorrectLetters.length >= 6;
+  const isWinner = wordToGuess
+    .split('')
+    .every((letter) => guessedLetters.includes(letter));
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return;
+
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+    },
+    [guessedLetters, isWinner, isLoser]
+  );
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+    document.addEventListener('keypress', handler);
+
+    return () => {
+      document.removeEventListener('keypress', handler);
+    };
+  }, [guessedLetters]);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (key !== 'Enter') return;
+
+      e.preventDefault();
+      setGuessedLetters([]);
+      setWordToGuess(getWord());
+    };
+    document.addEventListener('keypress', handler);
+
+    return () => {
+      document.removeEventListener('keypress', handler);
+    };
+  }, []);
 
   return (
     <div
@@ -20,10 +68,26 @@ function App() {
         margin: '0 auto',
         alignItems: 'center',
       }}>
-      <div style={{ fontSize: '2rem', textAlign: 'center' }}>Lose Win</div>
-      <HangmanDrawing />
-      <HangmanWord />
-      <Keyboard />
+      <div style={{ fontSize: '2rem', textAlign: 'center' }}>
+        {isWinner && 'Winner! - Refresh or click Enter to try again'}
+        {isLoser && 'Nice Try! - Refresh or click Enter to try again'}
+      </div>
+      <HangmanDrawing numberofGuesses={incorrectLetters.length} />
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={wordToGuess}
+      />
+      <div style={{ alignSelf: 'stretch' }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetter={guessedLetters.filter((letter) =>
+            wordToGuess.includes(letter)
+          )}
+          inActiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
+      </div>
     </div>
   );
 }
--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
-const HangmanWord = () => {
-  return <div>HangmanWord</div>;
+type HangmanWordProps = {
+  wordToGuess: string;
+  guessedLetters: string[];
+  reveal?: boolean;
+};
+
+const HangmanWord = ({
+  wordToGuess,
+  guessedLetters,
+  reveal = false,
+}: HangmanWordProps) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        gap: '.25em',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace',
+      }}>
+      {wordToGuess.split('').map((letter, index) => (
+        <span
+          style={{ borderBottom: '.1em solid black' }}
+          key={index}>
+          <span
+            style={{
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? 'visible'
+                  : 'hidden',
+              color:
+                !guessedLetters.includes(letter) && reveal ? 'red' : 'black',
+            }}>
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default HangmanWord;
